b430ee29a99f9628ced55049a087335b24a3c14d
Mark Watts <watts.mark2015@gmail.com>
2018-02-24 17:31:43 -0600

    Removing generated database files

--- modelcompletion/templatetags/dict_filter.py
+++ modelcompletion/templatetags/dict_filter.py
@@ -2,12 +2,13 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def lookup(value, arg):
-    print arg
-    x,y=arg.split(',')
-    print x,y
+    print(arg)
+    x, y = arg.split(',')
+    print(x, y)
     # if x.isdigit() and y.isdigit():
     #     if (int(x),int(y)) in value.keys():
     #         return value[int(x),int(y)]
-    # 
+    #
